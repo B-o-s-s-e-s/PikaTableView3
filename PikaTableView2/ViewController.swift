93f703bb673782ed9a56110a1e4b0840da0e4cab
pikaka <senuang.cai9h@gmail.com>
2021-07-31 09:45:03 -0400

    Initial Commit

---
+++ PikaTableView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PikaTableView2
+//
+//  Created by Pika Cai on 7/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
